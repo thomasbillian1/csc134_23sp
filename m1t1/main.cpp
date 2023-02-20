105d18b20745b1b8dec8c1a521144ea8f9485fba
thomasbillian1 <125482315+thomasbillian1@users.noreply.github.com>
2023-02-20 11:34:36 -0500

    module one training

---
+++ m1t1/main.cpp
@@ -0,0 +1,15 @@
+// M1T1 - Hello World
+// CSC 134
+// Thomas Billian
+// 1/9/2023
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // console out
+  cout << "Hello Thomas Billian!" << endl;
+  cout << "Welcome to CSC 134." << endl;
+  
+}

a829da3efc4f3f166245e2e9121bea4f8d27113b
thomasbillian1 <125482315+thomasbillian1@users.noreply.github.com>
2023-02-27 11:10:49 -0500

    started project

---
+++ M4T2_Billian/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    // print three line menu
+    //press 1 to blah etc
+    // cin an interger
+    return 0;
+}
